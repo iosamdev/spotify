a999d63f057336137ecc23975b791b10fc394010
Samuel <iosamdev@gmail.com>
2026-09-17 14:10:03 +0800

    Theme of Application

---
+++ lib/core/configs/theme/app_colors.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class appColors {
+
+  static const primary = Color(0xFF4CAF50);
+  static const lightBackground = Color(0xFFF5F5F5);
+  static const darkBackground = Color(0xFF333333);
+  static const grey = Color(0xFF9E9E9E);
+  static const darkGrey = Color(0xFF757575);
+}
